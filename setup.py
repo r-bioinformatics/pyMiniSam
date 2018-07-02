c9799b1f7a1e7de607d20be50dda380a6f290dfb
Anthony Fejes <apfejes@users.noreply.github.com>
2018-07-02 14:52:33 -0700

    more meta data in the setup.py

--- setup.py
+++ setup.py
@@ -2,4 +2,7 @@ from distutils.core import setup
 from Cython.Build import cythonize
 
 setup(name='pyMiniSam',
+      version=1.0,
+      author="apfejes",
+
       ext_modules=cythonize("sam_functions.pyx", annotate=True))
