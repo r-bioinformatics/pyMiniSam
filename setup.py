688511e7fb636def63661936993f0cb6fbebc724
Anthony Fejes <apfejes@users.noreply.github.com>
2018-07-02 14:36:39 -0700

    set name in setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 from distutils.core import setup
 from Cython.Build import cythonize
 
-setup(ext_modules=cythonize("sam_functions.pyx", annotate=True))
+setup(name='pyMiniSam',
+      ext_modules=cythonize("sam_functions.pyx", annotate=True))
