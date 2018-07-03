abdc02f56dbbe51c8fa29c199b0bdad7dedf3607
Anthony Fejes <apfejes@users.noreply.github.com>
2018-07-03 10:29:37 -0700

    More work on setup.py - hopefully a functioning build for use with other packages.

--- setup.py
+++ setup.py
@@ -4,8 +4,6 @@ from distutils.extension import Extension
 
 USE_CYTHON = True
 
-import sys
-
 cmd_class = {}
 ext_modules = []
 
@@ -13,24 +11,23 @@ if USE_CYTHON:
     try:
         from Cython.Distutils import build_ext
     except ImportError:
-        if USE_CYTHON=='auto':
-            USE_CYTHON=False
+        if USE_CYTHON == 'auto':
+            USE_CYTHON = False
         else:
             raise
 
 if USE_CYTHON:
     ext_modules += [
-        Extension("pyMiniSam.sam_functions", ["sam_functions.pyx"]),
+        Extension("sam_functions", ["sam_functions.pyx"]),
     ]
-    cmd_class.update({ 'build_ext': build_ext })
+    cmd_class.update({'build_ext': build_ext})
 else:
     ext_modules += [
-        Extension("pyMiniSam.sam_functions", ["sam_functions.pyx"]),
+        Extension("sam_functions", ["sam_functions.pyx"]),
     ]
 
 setup(name='pyMiniSam',
       version=1.0,
       author="apfejes",
-      cmd_class=cmd_class,
       long_description=open('README.txt').read(),
       ext_modules=cythonize(ext_modules, annotate=True))
