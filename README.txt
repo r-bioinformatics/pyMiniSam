6d0529670fbf0a412d71511740865fcc0d1482ed
Anthony Fejes <apfejes@users.noreply.github.com>
2018-07-03 10:11:28 -0700

    More work on setup.py - hopefully a functioning build for use with other packages.

---
+++ README.txt
@@ -0,0 +1,1 @@
+This is an experimental, cython version of pySAM, implemented based soleley on the SAM/BAM format document.
--- setup.py
+++ setup.py
@@ -1,8 +1,36 @@
 from distutils.core import setup
 from Cython.Build import cythonize
+from distutils.extension import Extension
+
+USE_CYTHON = True
+
+import sys
+
+cmd_class = {}
+ext_modules = []
+
+if USE_CYTHON:
+    try:
+        from Cython.Distutils import build_ext
+    except ImportError:
+        if USE_CYTHON=='auto':
+            USE_CYTHON=False
+        else:
+            raise
+
+if USE_CYTHON:
+    ext_modules += [
+        Extension("pyMiniSam.sam_functions", ["sam_functions.pyx"]),
+    ]
+    cmd_class.update({ 'build_ext': build_ext })
+else:
+    ext_modules += [
+        Extension("pyMiniSam.sam_functions", ["sam_functions.pyx"]),
+    ]
 
 setup(name='pyMiniSam',
       version=1.0,
       author="apfejes",
-
-      ext_modules=cythonize("sam_functions.pyx", annotate=True))
+      cmd_class=cmd_class,
+      long_description=open('README.txt').read(),
+      ext_modules=cythonize(ext_modules, annotate=True))
